1497945587c137fb608ebca04966883a84bd492f
tymsztrumpf <tymoteusztrumpf@gmail.com>
2023-07-31 21:08:56 +0200

    implementation of protected routes

--- powerboard/src/router/AppRouter.tsx
+++ powerboard/src/router/AppRouter.tsx
@@ -5,15 +5,24 @@ import LoginPage from "../pages/LoginPage";
 import React from "react";
 import BoardsPage from "../pages/BoardsPage";
 import BoardPage from "../pages/BoardPage";
+import {ProtectedRoute} from "./ProtectedRoute";
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/boards' element={<BoardsPage />} />
-            <Route path='/board/:id' element={<BoardPage />} />
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/signup' element={<SignupPage/>}/>
+            <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/boards' element={
+                <ProtectedRoute>
+                    <BoardsPage/>
+                </ProtectedRoute>
+            }/>
+            <Route path='/board/:id' element={
+                <ProtectedRoute>
+                    <BoardPage/>
+                </ProtectedRoute>
+            }/>
         </Routes>
     )
 }
